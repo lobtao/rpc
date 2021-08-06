435c6947e733e5a6472c24521ed16e1a69100a83
lobtao <lobtao@gmail.com>
2021-08-06 12:30:55 +0800

    add check parameter

--- src/BaseRpc.php
+++ src/BaseRpc.php
@@ -4,8 +4,8 @@ namespace lobtao\rpc;
 
 class BaseRpc
 {
-    public $ERR_MSG_PARAMS_ERROR = 'request parameter error';
-    public $ERR_MSG_CLASS_NOT_FOUND = '`%s` does not exist';
+    public $ERR_MSG_PARAMS_ERROR                = 'request parameter error';
+    public $ERR_MSG_CLASS_NOT_FOUND             = '`%s` does not exist';
     public $ERR_MSG_FUNCTION_NOT_FOUND_IN_CLASS = 'medthod `{0}` does not exist in class `{1}`';
 
     protected $namespace;
@@ -24,13 +24,21 @@ class BaseRpc
     public function handle($namespace, $func, $args, $authVerify = null)
     {
         $this->namespace = $namespace;
-        $this->func = $func;
-        $this->args = $args;
+        $this->func      = $func;
+        $this->args      = $args;
 
         // 微信小程序特别设置；浏览器提交过来自动转换
         if (gettype($this->args) == 'string') {
             $this->args = json_decode($this->args, true);
         }
+
+        // 检查参数格式
+        if (!empty($args)) {
+            if (!isset($args[0]) || gettype($args[0]) != 'array') {
+                throw new RpcException($this->ERR_MSG_PARAMS_ERROR);
+            }
+        }
+
 
         // 权限等验证过滤处理
         if (isset($authVerify)) {
@@ -54,9 +62,9 @@ class BaseRpc
         $params = explode('_', $func, 2);
         if (count($params) != 2) throw new RpcException($this->ERR_MSG_PARAMS_ERROR);
 
-        $serName = ucfirst($params[0]);
+        $serName   = ucfirst($params[0]);
         $className = $this->namespace . $serName . 'Service';
-        $funcName = $params[1];
+        $funcName  = $params[1];
         if (!class_exists($className)) throw new RpcException(sprintf($this->ERR_MSG_CLASS_NOT_FOUND, $className));
 
         $object = new $className();
